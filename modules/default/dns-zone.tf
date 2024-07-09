2a934da4179cd8d5d08afcb964f2f1429b9b1fc1
Peter Wiggers <peter@bitlayer.nl>
2024-07-09 11:07:50 +0200

    create A-record for lb in dns zone

--- modules/default/dns-zone.tf
+++ modules/default/dns-zone.tf
@@ -11,6 +11,14 @@ resource "azurerm_dns_a_record" "wildcard" {
   records             = azurerm_public_ip.ingress_ipv4.*.ip_address
 }
 
+resource "azurerm_dns_a_record" "lb" {
+  zone_name           = azurerm_dns_zone.default.name
+  resource_group_name = azurerm_resource_group.default.name
+  name                = "lb"
+  ttl                 = 300
+  records             = azurerm_public_ip.ingress_ipv4.*.ip_address
+}
+
 resource "azurerm_dns_a_record" "int_wildcard" {
   count = var.traefik_internal_loadbalancer_ip != "" ? 1 : 0
 
