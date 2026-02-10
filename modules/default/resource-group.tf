80bdc4c99eca5bc680dacd0d95fd717f827f90e5
Luuk Rost <rostl@wigo4it.nl>
2026-02-10 10:36:24 +0100

    check if RG exists

--- modules/default/resource-group.tf
+++ modules/default/resource-group.tf
@@ -1,4 +1,9 @@
+data azurerm_resource_group "default" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_resource_group" "default" {
+  count    = data.azurerm_resource_group.default.id == null ? 1 : 0
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
